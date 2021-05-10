82507406b80331974c01fcbe79ed91031874cc58
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-05-10 15:01:51 +0200

    Initial commit

---
+++ View/home.view.php
@@ -0,0 +1,14 @@
+
+<main class="flexColumn">
+
+    <img id="image" src="/assets/witcher-wallpaper.jpg" alt="witcher">
+
+    <h2 id="presentation">Des articles sur l'univer de the Witcher ! </h2>
+
+    <a id="homeArticle" href="?controller=articles">
+
+        <h3 class="titleImage">Voir tous les articles > </h3>
+        <img src="/assets/sorcelleur.png" alt="articles">
+
+    </a>
+</main>
